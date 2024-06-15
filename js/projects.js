df1df07bdd5a54f77f432628ed1a7edd8064897e
KayleeS <songspyspy@gmail.com>
2024-06-16 00:17:54 +1000

    [feat]contact me

--- js/projects.js
+++ js/projects.js
@@ -1,121 +1,15 @@
-var article = document.getElementById("article");
+var openNav = document.getElementById("open-nav");
+closeNav = document.getElementById("close-nav");
 subNav = document.getElementById("sub-nav");
-nav = document.getElementById("navigation");
-openNav = document.getElementById("open-nav");
 
-/** 2A picture */
-function A21Picture() {
-    var picture = document.getElementById("A2-1-full");
-    picture.style.display = "block";
-    article.style.display = "none";
+/** close sub-navigation */
+function closeSub() {
     subNav.style.display = "none";
-    nav.style.display = "none";
-    openNav.style.display = "none";
+    openNav.style.display = "block";
 }
 
-function closeA21Picture() {
-    var picture = document.getElementById("A2-1-full");
-    picture.style.display = "none";
-    article.style.display = "block";
-    nav.style.display = "block";
+/** open sub-navigation */
+function openSub() {
     subNav.style.display = "block";
     openNav.style.display = "none";
-    window.scrollTo(0, 400);
 }
-
-function A22Picture() {
-    var picture = document.getElementById("A2-2-full");
-    picture.style.display = "block";
-    article.style.display = "none";
-    subNav.style.display = "none";
-    nav.style.display = "none";
-    openNav.style.display = "none";
-}
-
-function closeA22Picture() {
-    var picture = document.getElementById("A2-2-full");
-    picture.style.display = "none";
-    article.style.display = "block";
-    nav.style.display = "block";
-    subNav.style.display = "block";
-    openNav.style.display = "none";
-    window.scrollTo(0, 400);
-}
-/** 2D picture */
-function D21Picture() {
-    var picture = document.getElementById("D2-1-full");
-    picture.style.display = "block";
-    article.style.display = "none";
-    subNav.style.display = "none";
-    nav.style.display = "none";
-    openNav.style.display = "none";
-}
-
-function closeD21Picture() {
-    var picture = document.getElementById("D2-1-full");
-    picture.style.display = "none";
-    article.style.display = "block";
-    nav.style.display = "block";
-    subNav.style.display = "block";
-    openNav.style.display = "none";
-    window.scrollTo(0, 3700);
-}
-
-function D22Picture() {
-    var picture = document.getElementById("D2-2-full");
-    picture.style.display = "block";
-    article.style.display = "none";
-    subNav.style.display = "none";
-    nav.style.display = "none";
-    openNav.style.display = "none";
-}
-
-function closeD22Picture() {
-    var picture = document.getElementById("D2-2-full");
-    picture.style.display = "none";
-    article.style.display = "block";
-    nav.style.display = "block";
-    subNav.style.display = "block";
-    openNav.style.display = "none";
-    window.scrollTo(0, 3700);
-}
-
-function D23Picture() {
-    var picture = document.getElementById("D2-3-full");
-    picture.style.display = "block";
-    article.style.display = "none";
-    subNav.style.display = "none";
-    nav.style.display = "none";
-    openNav.style.display = "none";
-}
-
-function closeD23Picture() {
-    var picture = document.getElementById("D2-3-full");
-    picture.style.display = "none";
-    article.style.display = "block";
-    nav.style.display = "block";
-    subNav.style.display = "block";
-    openNav.style.display = "none";
-    window.scrollTo(0, 5800);
-}
-
-function D24Picture() {
-    var picture = document.getElementById("D2-4-full");
-    picture.style.display = "block";
-    article.style.display = "none";
-    subNav.style.display = "none";
-    nav.style.display = "none";
-    openNav.style.display = "none";
-}
-
-function closeD24Picture() {
-    var picture = document.getElementById("D2-4-full");
-    picture.style.display = "none";
-    article.style.display = "block";
-    nav.style.display = "block";
-    subNav.style.display = "block";
-    openNav.style.display = "none";
-    window.scrollTo(0, 6000);
-}
-/** 2E picture */
-/** portfolio picture */
--- js/script.js
+++ js/script.js
@@ -14,19 +14,3 @@ function black(e) {
     y = e.clientY;
     canvas.style.opacity = 0.85;
 }
-
-var openNav = document.getElementById("open-nav");
-closeNav = document.getElementById("close-nav");
-subNav = document.getElementById("sub-nav");
-
-/** close sub-navigation */
-function closeSub() {
-    subNav.style.display = "none";
-    openNav.style.display = "block";
-}
-
-/** open sub-navigation */
-function openSub() {
-    subNav.style.display = "block";
-    openNav.style.display = "none";
-}
